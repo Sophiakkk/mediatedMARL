9787806789a11042252f0dfd58fd571441192135
Sophiakkk <zhuzhuhsm@gmail.com>
2024-08-03 23:30:57 -0400

    fix hydra argparser conflict

--- tabular-games/run.py
+++ tabular-games/run.py
@@ -12,12 +12,17 @@ from src.vanilla_mediator.controller.controller import EyeOfGodVanilla
 
 from utils_pkg.wandb_notion import commit_to_notion
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--array_task_id', type=int, default=0)
 
 @hydra.main(config_path='conf', config_name='config')
-
 def train(cfg):
+    # hydra could override the config with the one from the command line
+    # define the parser inside the function to avoid the conflict
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--array_task_id', type=int, default=0)
+    group = 'vanilla'
+    name = group + "_" + str(parser.parse_args().array_task_id)
+    wandb.init(project='Mediated_MARL_PD_vanilla', group=group, mode='online', name=name)
+
     cfg_notion = cfg.notion
     cfg = cfg.type
     cfg_wb = OmegaConf.to_container(cfg, resolve=True)
@@ -46,11 +51,8 @@ def train(cfg):
 
     info = zog.evaluate_policy(eval_episodes, env)
     # mean_step_reward, pick_mediator, policy_agents, policy_mediator, value_agents, value_mediator
+    wandb.finish()
     return info
 
 if __name__ == '__main__':
-    group = 'vanilla'
-    name = group + "_" + str(parser.parse_args().array_task_id)
-    wandb.init(project='Mediated_MARL_PD_vanilla', group=group, mode='online', name=name)
     train()
-    wandb.finish()
