35ef2c8f437d7297773a614da6a21de9714b9dcf
Sophiakkk <zhuzhuhsm@gmail.com>
2024-08-03 22:56:39 -0400

    add array task id

--- tabular-games/run.py
+++ tabular-games/run.py
@@ -12,8 +12,11 @@ from src.vanilla_mediator.controller.controller import EyeOfGodVanilla
 
 from utils_pkg.wandb_notion import commit_to_notion
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--array_task_id', type=int, default=0)
 
 @hydra.main(config_path='conf', config_name='config')
+
 def train(cfg):
     cfg_notion = cfg.notion
     cfg = cfg.type
@@ -46,8 +49,6 @@ def train(cfg):
     return info
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--array_task_id', type=int, default=0)
     group = 'vanilla'
     name = group + "_" + str(parser.parse_args().array_task_id)
     wandb.init(project='Mediated_MARL_PD_vanilla', group=group, mode='online', name=name)
