b579e4875f218f69221fb18bdc54aecee85381db
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-23 13:39:26 -0400

    rerun the 16 horizon

--- tabular-games/vector_run.sh
+++ tabular-games/vector_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=mediated_IPC
-#SBATCH --qos=cpu_qos
+#SBATCH --account=deadline
+#SBATCH --qos=deadline
+#SBATCH --job-name=IPC_mediator
 #SBATCH --mem=4G
 #SBATCH --time=0-24:00:00
-#SBATCH --partition=cpu
-#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:rtx6000:1
+#SBATCH --cpus-per-task=10
 #SBATCH --array=1-10
 
 python run.py
